ab15d058d2234b200a576ae5ccda80bd240cdbdf
AitorSanJose1 <62299935+AitorSanJose1@users.noreply.github.com>
2021-11-07 12:44:38 +0100

    Update apunteaAldaketaEgin.php

--- app/apunteaAldaketaEgin.php
+++ app/apunteaAldaketaEgin.php
@@ -5,12 +5,14 @@ session_start();
 
 $LDAP= $_SESSION['LDAP']; #lortu dugu aldatu nahi dugun erabiltzailearen LDAP
 
+#form-etik lortutako datuak
 $id= $_POST['id'];
 $irakasgaia= $_POST['irakasgaia'];
 $gradua= $_POST['gradua'];
 $kurtsoa= $_POST['kurtsoa'];
 $fitx= $_POST['fitxategia'];
 
+#datu basean apuntea sartutako datuekin eguneratzeko sql query-a
 $sql="UPDATE `Apunte` SET `Id`='$id',`IgotzeData`= CURDATE(),`Irakasgaia`='$irakasgaia',`Gradua`='$gradua',`Kurtsoa`='$kurtsoa',`Fitxategia`='$fitx',`ErabLDAP`='$LDAP' WHERE `Id`='$id'";
 $query = mysqli_query($conn, $sql);
 if($query){
