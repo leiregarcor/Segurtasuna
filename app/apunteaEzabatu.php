80ebbfa6ca5991c2354bb8f9afdadb86f5ed7262
AitorSanJose1 <62299935+AitorSanJose1@users.noreply.github.com>
2021-11-07 12:46:38 +0100

    Update apunteaEzabatu.php

--- app/apunteaEzabatu.php
+++ app/apunteaEzabatu.php
@@ -5,6 +5,7 @@ session_start();
 
 $id= $_POST['id']; #lortu dugu aldatu nahi dugun apuntearen Id
 
+#aukeratutako apuntea ezabatzeko sql query-a
 $apuntea = " DELETE FROM `Apunte` WHERE `Id` = '$id'";
 
 $rst=mysqli_query($conn,$apuntea);
