9a42ca9deb9e896134a54d0186b618cfd4f20fe8
aitor <aitorsanjoseheras@gmail.com>
2021-12-14 20:41:58 +0100

    DatuBase nueva

--- database.sql
+++ database.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: db
--- Tiempo de generación: 10-12-2021 a las 10:57:46
--- Versión del servidor: 10.6.5-MariaDB-1:10.6.5+maria~focal
+-- Tiempo de generación: 14-12-2021 a las 19:40:50
+-- Versión del servidor: 10.6.4-MariaDB-1:10.6.4+maria~focal
 -- Versión de PHP: 7.4.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -42,17 +42,22 @@ CREATE TABLE `Apunte` (
 --
 
 INSERT INTO `Apunte` (`Id`, `IgotzeData`, `Irakasgaia`, `Gradua`, `Kurtsoa`, `Fitxategia`, `ErabLDAP`) VALUES
-(1, '2021-11-30', 'DEA', 'Ingenieritza informatikoa', 2, 'https://drive.google.com/uc?id=1ZvEJpM8IYijT4Eq4QAOoP3qsnVYWU4q7&export=download', 961005),
-(11, '2021-11-03', 'LKSA', 'Ingenieritza informatikoa', 2, 'https://drive.google.com/uc?id=1RocaWbrogWonFGgiJCSBr7xxf9n8NAEF&export=downloadlink', 953985),
-(12, '2021-11-03', 'SI', 'Ingenieritza informatikoa', 2, 'https://drive.google.com/uc?id=1tV8Z1659L1MNWmMX1AKKuPH4WJtIU4Y3&export=download', 953985),
+(1, '2021-12-09', 'DEA', 'Ingenieritza informatikoa', 2, 'https://drive.google.com/uc?id=1ZvEJpM8IYijT4Eq4QAOoP3qsnVYWU4q7&export=download', 961005),
+(3, '2021-10-19', 'Mecanica de fluidos', 'Ingenieritza Mekanikoa', 2, 'link00', 756423),
+(4, '2021-10-19', 'Mecanica de fluidos', 'Ingenieritza Mekanikoa', 2, 'link01', 756423),
+(8, '2021-11-02', 'KIS', 'Ingenieritza Mekanikoa', 5, 'LINK7', 756423),
+(9, '2021-11-02', 'EA', 'Ingenieritza Mekanikoa', 5, 'linketo', 756423),
+(10, '2021-11-02', 'SI', 'Ingenieritza Mekanikoa', 5, 'linketo', 756423),
+(11, '2021-11-03', 'LKSA', 'Ingenieritza informatikoa', 2, 'https://drive.google.com/uc?id=1RocaWbrogWonFGgiJCSBr7xxf9n8NAEF&export=downloadlink', 911222),
+(12, '2021-11-03', 'SI', 'Ingenieritza informatikoa', 2, 'https://drive.google.com/uc?id=1tV8Z1659L1MNWmMX1AKKuPH4WJtIU4Y3&export=download', 911222),
+(13, '2021-11-03', 'SEO', 'Ingenieritza informatikoa', 2, 'https://drive.google.com/uc?id=1G6NpA6RaCLV3bxNzdxPg1l0h-hy4-6ps&export=download', 911222),
 (14, '2021-11-04', 'KSO', 'Ingenieritza informatikoa', 2, 'https://drive.google.com/uc?id=1jZnhhPZD76bx3rpwfdpWEAZtkLBv-SdQ&export=download', 961005),
-(15, '2021-11-04', 'IO', 'Ingenieritza Mekanikoa', 2, 'https://drive.google.com/uc?id=1kt5XhDmD1nXC4s-Ivod9-4C4ENFq5Pz1&export=download', 953989),
+(15, '2021-11-04', 'IO', 'Ingenieritza informatikoa', 2, 'https://drive.google.com/uc?id=1kt5XhDmD1nXC4s-Ivod9-4C4ENFq5Pz1&export=download', 961005),
 (16, '2021-11-04', 'DB', 'Ingenieritza informatikoa', 2, 'https://drive.google.com/uc?id=1zrlhes5FwvQjcNu1EVOJqYg1AXBoYmUG&export=download', 961005),
 (17, '2021-11-04', 'Mate diskretua', 'Ingenieritza informatikoa', 1, 'https://drive.google.com/uc?id=1cJHstZSy2NcyvxcYq4lnVsyi5vGMfRis&export=download', 961005),
-(18, '2021-11-04', 'Materialak', 'Ingenieritza Industriala', 2, 'https://i.ytimg.com/vi/UO2EAfB6pTE/maxresdefault.jpg', 953986),
-(19, '2021-11-04', 'Elektronikaren oinarriakII', 'Ingenieritza Elektronikoa', 2, 'https://www.youtube.com/watch?v=T5xzmkz87v0', 953986),
-(21, '2021-11-04', 'Elektronikaren oinarriak', 'Ingenieritza Elektronikoa', 1, 'https://www.youtube.com/watch?v=T5xzmkz87v0', 953987),
-(22, '2021-11-04', 'materialak', 'Ingenieritza Mekanikoa', 3, 'https://drive.google.com/uc?id=1kt5XhDmD1nXC4s-Ivod9-4C4ENFq5Pz1&export=download', 961005);
+(18, '2021-11-04', 'Materialak', 'Ingenieritza Industriala', 2, 'https://i.ytimg.com/vi/UO2EAfB6pTE/maxresdefault.jpg', 235198),
+(19, '2021-11-04', 'Elektronikaren oinarriak', 'Ingenieritza Industriala', 1, 'https://www.youtube.com/watch?v=T5xzmkz87v0', 235198),
+(21, '2021-11-04', 'Elektronikaren oinarriak', 'Ingenieritza Elektronikoa', 1, 'https://www.youtube.com/watch?v=T5xzmkz87v0', 911222);
 
 -- --------------------------------------------------------
 
@@ -62,7 +67,7 @@ INSERT INTO `Apunte` (`Id`, `IgotzeData`, `Irakasgaia`, `Gradua`, `Kurtsoa`, `Fi
 
 CREATE TABLE `Erabiltzaile` (
   `LDAP` int(11) NOT NULL,
-  `Pasahitza` varchar(45) DEFAULT NULL,
+  `Pasahitza` varchar(200) DEFAULT NULL,
   `Izena` varchar(45) DEFAULT NULL,
   `Abizena` varchar(45) DEFAULT NULL,
   `NAN` varchar(9) DEFAULT NULL,
@@ -70,19 +75,19 @@ CREATE TABLE `Erabiltzaile` (
   `Mail` varchar(45) DEFAULT NULL,
   `JaioData` date DEFAULT NULL,
   `Gradua` varchar(45) DEFAULT NULL,
-  `BankuZenb` int(20) NOT NULL
+  `BankuKontu` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `Erabiltzaile`
 --
 
-INSERT INTO `Erabiltzaile` (`LDAP`, `Pasahitza`, `Izena`, `Abizena`, `NAN`, `Telefono`, `Mail`, `JaioData`, `Gradua`, `BankuZenb`) VALUES
-(953985, '32d064da8114739ce7ba481b78a8f868', 'Leire', 'Garcia', '16102927Y', 660688416, 'leiregarciaupv@gmail.com', '2001-04-30', 'Ingenieritza informatikoa', 0),
-(953986, '202cb962ac59075b964b07152d234b70', 'Ane', 'Martinez', '79135423J', 987654321, 'ane@gmail.com', '2021-10-25', 'Ingenieritza Industriala', 0),
-(953987, '32d064da8114739ce7ba481b78a8f868', 'Aner', 'Lopez', '79269379V', 987654321, 'aner@gmail.com', '2019-10-28', 'Ingenieritza Elektronikoa', 0),
-(953989, '202cb962ac59075b964b07152d234b70', 'Leire', 'Gonzalez', '16103278N', 987654321, 'lei@gmail.com', '2019-09-10', 'Ingenieritza Mekanikoa', 0),
-(961005, 'c893bad68927b457dbed39460e6afd62', 'Aitor', 'San José', '45893403P', 688875743, 'aitorsanjoseheras@gmail.com', '2001-01-28', 'Ingenieritza informatikoa', 0);
+INSERT INTO `Erabiltzaile` (`LDAP`, `Pasahitza`, `Izena`, `Abizena`, `NAN`, `Telefono`, `Mail`, `JaioData`, `Gradua`, `BankuKontu`) VALUES
+(235198, '$2y$10$ANbiwsKBsF.IN04Ok1itqeXtclrlQWqUKuXXtKprNUBf60EK0tQdu', 'Enzo', 'Fernandez', '09876542C', 657834292, 'enzofernandez@gmail.com', '2021-12-08', 'Ingenieritza Industriala', 'DAasSzDZFYOILq4qxFOLR/DBS9QrOaWqDa6RUQLvOsg='),
+(756423, '$2y$10$bkxznTXdVMA179WmmACROOQwX19.Y9O40tpODfpurx7vIg2256rWm', 'Ander', 'Prieto', '11916226H', 948576321, 'anderpri@gmail.com', '2021-12-12', 'Ingenieritza Mekanikoa', 'E4vSvRfeVuYzMK0GW+gWY8mXc2oPY1Ldd+vHCfpiHlQ='),
+(900000, '$2y$10$zUP2y3bsIJJYa/ImQ.RR.eWzWxYyLLtAbvPLSRXpudJ/renF2eKCC', 'Unai', 'Gonzalez', '11223344B', 999999999, 'example@gmail.com', '2021-12-02', 'Ingenieritza informatikoa', 'v/fQZzGdjbTh5+DyXNauREKeKjhbwfe2TbYcxNx2Ftg='),
+(911222, '$2y$10$Ho9PCZrGhZ2tV7VeEEaoLu3JBPYkC41xgO9Wf4pPg3OpfU6pwHnLS', 'Jon', 'Blanco', '45950218J', 987351712, 'example@gmail.com', '2021-12-03', 'Ingenieritza informatikoa', '0'),
+(961005, '$2y$10$CPDTqubl1Q08AbGiL4Ylj.1nqPu2LlhBwGNgekriZQFwQCFyU7yQi', 'Aitor', 'San José', '45893403P', 688875743, 'aitorsanjoseheras@gmail.com', '2001-01-28', 'Ingenieritza informatikoa', '/7jdD3ZrA4PUdmUtcroerHBX7y/5apqRwQ4maGYiKSA=');
 
 --
 -- Índices para tablas volcadas
@@ -109,7 +114,7 @@ ALTER TABLE `Erabiltzaile`
 -- AUTO_INCREMENT de la tabla `Apunte`
 --
 ALTER TABLE `Apunte`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Restricciones para tablas volcadas
